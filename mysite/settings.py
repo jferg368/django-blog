78d8af7e0e187ab57fe8c4aba271e6f0d9bd0046
Jay Ferguson <fergujo@amazon.com>
2020-05-31 22:34:38 -0700

    Ran manage.py collectstatic

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,4 +115,5 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 LOGIN_URL = "/login/"
+STATIC_ROOT = "/Users/fergujo/UWPythonCertificate/Python230/assignments/django-blog/mysite/blogging/static"
 LOGIN_REDIRECT_URL = "/"
