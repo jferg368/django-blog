a5dca676606f740c5547fbcc42be20700c4125b4
Jay Ferguson <fergujo@amazon.com>
2020-05-31 19:13:02 -0700

    Remove favico logic.

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from django.views.generic.base import RedirectView
-from django.contrib.staticfiles.storage import staticfiles_storage
 from blogging.views import list_view, detail_view
 
 urlpatterns = [
     path("", list_view, name="blog_index"),
     path("posts/<int:post_id>/", detail_view, name="blog_detail"),
-path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico')))
 ]
